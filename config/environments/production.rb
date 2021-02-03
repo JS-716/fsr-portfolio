dff535145fb3f78c5687c9582534fcef878682f5
Jasmine Seren Rosso <jasmine.serenrosso@ieseg.fr>
2021-02-03 15:35:26 +0100

    Modify prod env for MailForm

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com'
-    port: 587
-    domain: 'gmail.com'
-    authentication: 'plain'
-    enable_starttls_auto: true
-    user_name: ENV['EMAIL_USERNAME']
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['EMAIL_USERNAME'],
     password: ENV['EMAIL_PASSWORD']
   }
 
